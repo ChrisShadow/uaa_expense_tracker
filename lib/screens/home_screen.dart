b8e9dad224d98eafc54fad2d7b25681f8898011e
ChrisShadow <chrisdanielbenee@gmail.com>
2024-05-31 20:48:54 -0400

    dashboard home: parte9,transaction 1:33min/2hs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -60,7 +60,7 @@ class _HomeState extends State<Home> {
         child: Column(
           children: [
             HeroCard(userId: userId,),
-            TransactCard()
+            const TransactCard()
           ],
         ),
       ),
